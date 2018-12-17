2b98c67731c05b738904152051c65ef0bcf9542e
Venkata Rathnam Muralidharan <imvenkat1997@gmail.com>
2018-12-17 21:30:08 +0630

    Classifier evaluation (#21)
    * #5 Issue
    
    * #20 Issue

--- fake.py
+++ fake.py
@@ -17,7 +17,9 @@ category = category.drop('REAL', 1)
 
 category = category.values
 
-x_train = news_clean_encoded
+#Split Data
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(news_clean_encoded, category, stratify=category, test_size = 0.20, random_state = 15)
 input_dimen = x_train.shape[1]
 
 # Model Definition
@@ -41,8 +43,6 @@ clf.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid')
 
 clf.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-y_train = category
-
 #Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0, 1))
@@ -61,6 +61,37 @@ accuracy_fn = track.history['acc']
 plt.plot(accuracy_fn)
 plt.title("Model Accuracy")
 plt.xlabel("Epochs")
+plt.show()
+
+#Evaluate Model
+x_test = sc.fit_transform(x_test)
+score = clf.evaluate(x = x_test, y = y_test, batch_size = 100, verbose = 1)
+
+print ("Evaluation loss: " + str(score[0]))
+print ("Evaluation acc: " + str(score[1]))
+
+from sklearn.metrics import confusion_matrix
+prediction = clf.predict(x_test)
+labels = [0, 1]
+con_mat = confusion_matrix(y_true = y_test, y_pred = np.round(prediction), labels = labels)
+
+print ("Confusion Matrix")
+print (con_mat)
+
+from sklearn.metrics import classification_report
+labels = ['Real', 'Fake']
+print (classification_report(y_true = y_test, y_pred = np.round(prediction), target_names = labels))
+
+from sklearn.metrics import roc_curve, auc
+fal_pos_rate, tru_pos_rate, threshold = roc_curve(y_test, prediction)
+
+area_under_curve = auc(fal_pos_rate, tru_pos_rate)
+plt.figure()
+plt.plot(fal_pos_rate, tru_pos_rate, label='AUC={:.3f}'.format(area_under_curve))
+plt.legend(loc='best')
+plt.xlabel('False Positive Rate (1-Specificity)')
+plt.ylabel('True Positive Rate (Sensitivity)')
+plt.title('ROC curve')
 plt.show()
 
 #Save Model
